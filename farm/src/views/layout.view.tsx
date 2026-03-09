565807915e8c15dd7557c3e2f395f30be1286a6d
tan <cupitrh@gmail.com>
2026-03-09 08:30:32 +0800

    fix(farm): enforce and apply silk theme

--- farm/src/views/layout.view.tsx
+++ farm/src/views/layout.view.tsx
@@ -19,6 +19,31 @@ const LayoutRoot: FC<{
           rel="stylesheet"
           type="text/css"
         />
+        <style>{`
+[data-theme="silk"] {
+  --p: 267 83% 74%;
+  --pc: 0 0% 100%;
+  --s: 318 72% 72%;
+  --sc: 0 0% 100%;
+  --a: 191 90% 52%;
+  --ac: 0 0% 100%;
+  --n: 243 29% 15%;
+  --nc: 0 0% 100%;
+  --b1: 270 100% 99%;
+  --b2: 270 80% 97%;
+  --b3: 268 56% 94%;
+  --bc: 248 32% 18%;
+  --in: 200 98% 39%;
+  --inc: 0 0% 100%;
+  --su: 158 64% 40%;
+  --suc: 0 0% 100%;
+  --wa: 35 92% 50%;
+  --wac: 0 0% 100%;
+  --er: 0 72% 51%;
+  --erc: 0 0% 100%;
+  color-scheme: light;
+}
+        `}</style>
       </head>
       <body class="bg-base-200 min-h-screen">
         <main class="max-w-6xl mx-auto p-4 md:p-6">
